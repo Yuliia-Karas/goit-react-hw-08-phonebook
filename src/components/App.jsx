972022574c13ad693331ed9a8de1b9e77bbd771a
Yuliia Karas <iuliiakaras@gmail.com>
2023-05-31 13:41:42 +0200

    step #5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,32 +2,35 @@ import React, { Component } from 'react';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import { Filter } from './Filter/Filter';
-import css from './App.module.css'
+import css from './App.module.css';
 import contacts from '../static/contacts.json';
-
 
 class App extends Component {
   constructor(props) {
     super(props);
- 
+
     this.state = {
       contacts,
       filter: '',
-          };
+    };
   }
 
-
-  addContact = (contact) => {
-    this.setState((prevState) => ({
-      contacts: [...prevState.contacts, contact]
-    }));
+  addContact = contact => {
+    console.log(contact);
+    if (this.state.contacts.find(cont => cont.name === contact.name)) { 
+      alert(`${contact.name} is already in contacts`);
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, contact],
+      }));
+    }
   };
 
-  handleChangeFilter = (event) => {
+  handleChangeFilter = event => {
     this.setState({ filter: event.target.value });
   };
-  
-getFilterContacts = () => {
+
+  getFilterContacts = () => {
     const { filter, contacts } = this.state;
     const filterlowerCase = filter.toLowerCase();
     return contacts.filter(({ name }) =>
@@ -39,21 +42,17 @@ getFilterContacts = () => {
     const { filter } = this.state;
     const FilterContacts = this.getFilterContacts();
 
-  return (
-    <div className={css.container}>
-      <h1>Phonebook</h1>
-      <ContactForm addContact={this.addContact } />
+    return (
+      <div className={css.container}>
+        <h1>Phonebook 5</h1>
+        <ContactForm addContact={this.addContact} />
 
-       
-      <h2>Contacts</h2>
-      <Filter value={filter} handleChangeFilter={this.handleChangeFilter} />
-      <ContactList contacts={FilterContacts} />
-      
-    </div>
-  );
+        <h2>Contacts</h2>
+        <Filter value={filter} handleChangeFilter={this.handleChangeFilter} />
+        <ContactList contacts={FilterContacts} />
+      </div>
+    );
+  }
 }
-}
-
 
 export default App;
-  
